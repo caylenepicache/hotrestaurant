fc45779f23bcf98dcbb3c0194a7867e4091e65de
Roarke Catubig <roarkecatubig@gmail.com>
2018-04-26 19:43:40 -0700

    Routed html files

--- app/routes/htmlRoutes.js
+++ app/routes/htmlRoutes.js
@@ -3,17 +3,23 @@ var path = require("path");
 
 module.exports = function(app){
     //Routes directly to the survey.html 
-    app.get('/survey', function (req, res) {
-      res.sendFile(path.join(__dirname + '/../public/survey.html'));
-    });
-    //Routes directly to the home page
     app.get('/', function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'))
+      res.sendFile(path.join(__dirname + '/../../public/home.html'));
     });
     //Accounts for any unexpected inputs following the '/' in the base url
     app.get('/:unknown', function (req, res) {
-        res.sendFile(path.join(__dirname + '/../public/home.html'))
+        res.sendFile(path.join(__dirname + '/../../public/home.html'))
     });
+    //Routes directly to the home page
+    app.get('/tables', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../../public/tables.html'))
+    });
+
+    app.get('/reservations', function (req, res) {
+        res.sendFile(path.join(__dirname + '/../../public/reservations.html'))
+    });
+
+
   
 };
   
