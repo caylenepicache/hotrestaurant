a98bb2c7c0bf32bea0077d5acd26e954ae73a2c6
Shane Drosi <ShaneDrosi@Joshuas-MacBook-Air.local>
2018-04-26 19:41:13 -0700

    this

--- app/server.js
+++ app/server.js
@@ -1,27 +1,22 @@
-// Dependencies
-// =============================================================
-var express = require("express");
-var bodyParser = require("body-parser");
-var path = require("path");
+//Dependencies:
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
 
-// Sets up the Express App
-// =============================================================
 var app = express();
-// Use the PORT designation immediately below this line for deploying to heroku.
-var PORT = process.env.PORT || 3000;
+var PORT = 3000;
 
-// Use the PORT designation immediately below this line for local testing.
-// var PORT = 3000;
-
-// Handles data parsing
-app.use(bodyParser.urlencoded({ extended: true }));
+// Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
-// Different modules needed for API routes and HTML routes
-// require('./app/routing/apiRoutes.js')(app); 
-// require('./app/routing/htmlRoutes.js')(app);
+//ROUTER
+require('./app/routes/apiRoutes.js')(app); 
+require('./app/routes/htmlRoutes.js')(app);
 
-// Initializes the server to begin listening for requests
-app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log('App listening on PORT: ' + PORT);
 });
